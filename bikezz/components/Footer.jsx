bbac022009ce12d93778d085555ae9f25a176945
Ahmad Jamal <ahmadjamal6886@gmail.com>
2024-07-30 16:09:33 +0500

    Update

--- bikezz/components/Footer.jsx
+++ bikezz/components/Footer.jsx
@@ -8,7 +8,33 @@ const Footer = () => {
   return (
     <footer className="pt-12 xl:pt-24 bg-primary text-white text-center">
       <div className="container mx-auto">
-        <div className="flex"></div>
+        <div className="flex flex-col gap-8">
+          <div>
+            <h2 className="capitalize leading-tight mb-2 ">
+              Subscribe to our newsletter
+            </h2>
+            <p className="text-white/60"> Be the first to get the latest news about trends, promotions, and much more!</p>
+          </div>
+          {/* form */}
+          <form className='flex flex-col xl:flex-row w-full max-w-[720px] mx-auto gap-5'>
+  <input placeholder='Your email address' />
+  <button className='btn w-full xl:max-w-[150px] h-[60px] btn-accent'>
+    Join
+  </button>
+</form>
+{/* social */}
+
+<div>
+  <Link>
+  
+  </Link>
+</div>
+        </div>
+         </div>
+         {/* copyright */}
+
+         <div className="py-6 border-t border-white/5 text-white/60">
+          Copyright &copy; 2024 Bikezz. All rights reserved.
          </div>
     </footer>
   )
