bcb7f218d200bbdb6675ce2aa3d2c8199a4150a1
Ahmad Jamal <ahmadjamal6886@gmail.com>
2024-07-30 16:52:43 +0500

    Update Error & Success Page

--- bikezz/app/stripe/success/page.jsx
+++ bikezz/app/stripe/success/page.jsx
@@ -1,9 +1,23 @@
-import Link from "next/link";
+ import Link from "next/link";
 
 const SuccessPage = () => {
   return (
-    <div>Success Page</div>
+    <section className='py-72'>
+      <div className="container mx-auto">
+        <h3 className="text-center mb-4">
+          Your Payment was successfull! Thank you!
+        </h3>
+        <Link href='/'
+        >
+        <button className="btn btn-primary mx-auto"> 
+          Back to the homepage
+        </button>
+        </Link>
+      </div>
+    </section>
   )
-}
+};
 
 export default SuccessPage;
+
+
